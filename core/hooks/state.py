e1fb6a1b7f4c25208a9500907913484746c22c38
gsanto <espiritosanto.gabriel@gmail.com>
2022-06-07 00:02:43 -0300

    testing

--- core/hooks/state.py
+++ core/hooks/state.py
@@ -4,5 +4,6 @@ from core.singletons.store import Store
 class State:
     def __init__(self, value, modifier) -> None:
         self.value = value
-        self.modifier = modifier
+        # self.modifier =  modifier
+        self.modifier = lambda _value: modifier(_value)
         Store().addState(self)
